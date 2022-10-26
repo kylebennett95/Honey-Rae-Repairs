27ead49517d1530061c922576ec363191fc789fb
unknown <kylebennett9514@gmail.com>
2022-10-25 23:25:19 -0500

    added search functionality, employee and customer views

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -2,16 +2,40 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./tickets.css"
 
-export const TicketList = () => {
+export const TicketList = ({ searchTermState }) => {
     const [tickets, setTickets] = useState([])
     const [filteredTickets, setFiltered] = useState([])
     const [emergency, setEmergency] = useState(false)
     const [openOnly, updateOpenOnly] = useState(false)
     const navigate = useNavigate()
-
+//local honey user gets the honey_user string from login.js, which points to the users/email section and gets the id and isStaff objects
     const localHoneyUser = localStorage.getItem("honey_user")
     const honeyUserObject = JSON.parse(localHoneyUser)
 
+    useEffect(
+        () => {
+            const searchedTickets = tickets.filter(ticket => {
+                return ticket.description.toLowerCase().startsWith(searchTermState.toLowerCase())
+            })
+            setFiltered(searchedTickets)
+        },
+        [ searchTermState ]
+    )
+
+    // This is what goes into the json file, goes to service tickets and assigns the value to tickets on line 6. The value is everything in servicetickets
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/serviceTickets`)
+            .then(response => response.json())
+            .then((ticketArray) => {
+              setTickets(ticketArray)
+            })
+        },
+        [] // When this array is empty, you are observing initial component state
+    )
+
+
+    // This line filters through the tickets, and applies the emergency true tickets to filteredTickets, otherwise just add it to tickets
     useEffect(
         () => {
             if (emergency) {
@@ -25,17 +49,7 @@ export const TicketList = () => {
         [emergency]
     )
 
-    useEffect(
-        () => {
-            fetch(`http://localhost:8088/serviceTickets`)
-    .then(response => response.json())
-    .then((ticketArray) => {
-     setTickets(ticketArray)
-     })
-        },
-        [] // When this array is empty, you are observing initial component state
-    )
-
+    //If an employee is signed in, the button returns all tickets. If it is a customer, it matches the userId to the users/id and returns only tickets that match
     useEffect(
         () => {
         if (honeyUserObject.staff) {
@@ -51,6 +65,7 @@ export const TicketList = () => {
     [tickets]
     )
 
+//This goes through the tickets array, matches the id to the userId and returns the ticket if the dateCompleted field is empty. If that field has a value it returns the ticket
     useEffect(
         () => {
             if (openOnly) {
@@ -67,6 +82,7 @@ export const TicketList = () => {
     [openOnly]
 )
 
+//Below, when the emergency buttons are clicked, it sets the set Emergency state to true and shows only emergency tickets. Or sets it to false and shows all tickets. Or when viewed by a customer, it sets openOnly tickets to true and shows open tickets, false and shows all or you can use the react navigate function to create a ticket
     return <>
     {
         honeyUserObject.staff
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,24 +1,17 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { TicketForm } from "../tickets/TicketForm"
-import { TicketList } from "../tickets/TicketList"
+import { CustomerViews } from "./CustomerViews"
+import { EmployeeViews } from "./EmployeeViews"
 
 export const ApplicationViews = () => {
-	return (
-        <Routes>
-            <Route path="/" element={
-                <>
-                    <h1>Honey Rae Repair Shop</h1>
-                    <div>Your one-stop-shop to get all your electronics fixed</div>
 
-                    <Outlet />
-                </>
-            }>
+    const localHoneyUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneyUser)
 
-                <Route path="tickets" element={ <TicketList /> } />
+    if (honeyUserObject.staff) {
 
-                <Route path="ticket/create" element={ <TicketForm /> } />
-            </Route>
-        </Routes>
-    )
+        return <EmployeeViews />
+    }
+    else {
+
+        return <CustomerViews />
+    }
 }
-
